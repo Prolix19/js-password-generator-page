3f765ed7a7699d7b8f6ac10efdc31955b9c60747
Prolix <51418825+Prolix19@users.noreply.github.com>
2022-01-16 20:46:24 -0600

    Comment JS file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 // Assignment code here
+
+// Object for variables and methods related to the password generation
 var passwordObject = {
     requestedLength: 0,
     includedChars: "",
@@ -11,12 +13,19 @@ var passwordObject = {
 }
 
 var generatePassword = function() {
+    // Must reset object for multiple clicks of the "Generate Password" button to work properly
     passwordObject.reset();
+
+    // Prompt user for the length from the acceptance criteria
     passwordObject.requestedLength = parseInt(window.prompt("Enter the desired length of the password (8-128 characters):"));
+
+    // Check length and, if invalid, have the user try again
     if(passwordObject.requestedLength < 8 || passwordObject.requestedLength > 128) {
         window.alert("Error: length must be a number from 8 through 128. Please try again.");
         return generatePassword();
     }
+
+    // Prompt user for which of the 4 character types should be included in the password
     if(window.confirm("Include lowercase characters in the password?")) {
         passwordObject.includedChars = "abcdefghijklmnopqrstuvwxyz";
     }
@@ -29,13 +38,22 @@ var generatePassword = function() {
     if(window.confirm("include special characters in the password?")) {
         passwordObject.includedChars = passwordObject.includedChars + " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
     }
+
+    /*
+        If user chose no character sets, includedChars will still be an empty string (falsy value)
+        So test !includedChars. If true, they did not select at least 1 character type. Have the user try again
+    */
     if(!passwordObject.includedChars) {
         window.alert("Error: at least one character type must be selected. Please try again.");
         return generatePassword();        
     }
+
+    // Select a random character from the selected list, and append it to the password until desired length is reached
     for(var i = 0; i < passwordObject.requestedLength; i++) {
         passwordObject.passwordText = passwordObject.passwordText + passwordObject.includedChars.charAt(Math.floor(Math.random() * passwordObject.includedChars.length));
     }
+    
+    // Return the randomly-generated password so writePassword() can display it in the HTML textarea
     return passwordObject.passwordText;
 };
 
